1dd782fe225d1e3154fef15ad8af3592d48d12cc
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-09-28 21:15:19 +0900

    테스트 문제 풀이

---
+++ src/main/java/nhn2019/quiz1/Main.java
@@ -0,0 +1,34 @@
+package nhn2019.quiz1;//Please don't change class name 'Main'
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int num = Integer.parseInt(input);
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        for (int i = 1; i < num; i++) {
+            if (treeSet.contains(i))
+                break;
+
+            if (num % i == 0) {
+                treeSet.add(i);
+                treeSet.add(num / i);
+            }
+        }
+
+        String result = treeSet.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+
+    }
+}
